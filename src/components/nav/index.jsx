48d2c8db78fcc4e0a000b0513e7b2abc1abdbbbf
santi <snt.v.01@gmail.com>
2017-09-19 10:44:06 +0200

    Nav component added

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import ReactDom from 'react-dom'
-
-import styles from './nav.scss'
 
 const Nav = () => {
   return (
-    <nav className=(styles.nav)>
-      <Title/>
-      <Seeker/>
-    </nav>
+    <h1>Nav</h1>
   )
 }
 
