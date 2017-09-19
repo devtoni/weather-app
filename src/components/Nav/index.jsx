875e6e71b4f3cc7009d5316241c7bf26d2ab5f8e
santi <snt.v.01@gmail.com>
2017-09-19 10:58:11 +0200

    Nav finally fixed

---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import style from './nav.scss'
+
+const Nav = () => {
+  return (
+    <nav className=(style.nav)>
+      <Title/>
+      <Seeker/>
+    </nav>
+  )
+}
+
+export default Nav
