ca4ea1bb39e6ea1b4de96f67316289dce03faae8
Marc Vergara <marc.vergara.f@gmail.com>
2017-09-19 13:30:56 +0200

    Nav done, form to search done

--- src/components/Seeker/index.jsx
+++ src/components/Seeker/index.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { NavLink } from 'react-router-dom'
 
 import style from './style.scss'
 
 
-const Seeker = () => {
+class Seeker extends Component {
 
-	return(
-		<form>
-		<input type="text"></input>
-		<button type="submit" className={style.button}>Search</button>
-		</form>
-		)
+	constructor(props){
+		super(props)
+		this.state = { city: ''}
+	}
+
+	handleChange = (e) => {
+		const value = e.target.value
+		this.setState({ city: value })
+	}
+
+	onSubmit () {
+		return '/forecast?city='+this.state.city;
+	}
+
+	render(){
+		const {id, onSubmit} = this.props
+		return(
+			<form>
+			<input id="city" name="city" type="text" placeholder='City to search' value={this.state.city} onChange={this.handleChange}></input>
+			<button><a type="submit" disabled={!this.state.city} href={this.onSubmit()}>Search</a></button>
+			</form>
+			)
+	}
 
 }
 
