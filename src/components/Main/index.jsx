3702b26c59d8c8b9571ef5f8108f11ee05c81bd2
Didac Tey <t_didac18@hotmail.com>
2017-09-19 12:54:42 +0200

    modify main.jsx

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './main.scss'
+import Seeker from '../Seeker'
 
 const Main = () => {
   return (
-    <div>
+    <div className={styles.contentMain}>
       <h1>Enter a City and State</h1>
+      <Seeker />
     </div>
   )
 }
